28c5b65cd8e714d376541017afdf2281f46ab437
Ernest <ernest.wambugu@student.moringaschool.com>
2023-07-06 11:20:14 +0300

    Done with migrations and seeding

--- db/migrate/20230706080520_create_hero_powers.rb
+++ db/migrate/20230706080520_create_hero_powers.rb
@@ -1,10 +1,9 @@
 class CreateHeroPowers < ActiveRecord::Migration[7.0]
   def change
     create_table :hero_powers do |t|
-      t.references :hero, null: false, foreign_key: true
-      t.references :power, null: false, foreign_key: true
       t.string :strength
-
+      t.belongs_to :heros, null: false, foreign_key: true
+      t.belongs_to :powers, null: false, foreign_key: true
       t.timestamps
     end
   end
